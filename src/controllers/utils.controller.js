5ac6023bf436c315f1171c7f3a5077243fc743bd
Swapnil Opash <sra.opash@gmail.com>
2023-07-17 12:46:42 +0530

    log added

--- src/controllers/utils.controller.js
+++ src/controllers/utils.controller.js
@@ -88,6 +88,7 @@ exports.getFiles = (req, res) => {
 
     if (files && files.length > 0) {
       files.forEach((file) => {
+        console.log(file);
         fileInfos.push({
           name: file,
           url:
