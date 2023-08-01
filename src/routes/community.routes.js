7da3711ae910e7ff93185bc690dd3bac3d4988cf
Swapnil Opash <sra.opash@gmail.com>
2023-08-01 18:54:12 +0530

    get members api changes

--- src/routes/community.routes.js
+++ src/routes/community.routes.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const communityController = require("../controllers/community.controller");
 const utilsController = require("../controllers/utils.controller");
 
+router.get("/", communityController.getCommunity);
 router.get("/approve-community", communityController.findApproveCommunity);
 router.get("/un-approve-community", communityController.findUnApproveCommunity);
 router.get("/search", communityController.search);
 router.get("/:id", communityController.findCommunityById);
 router.get("/status/:id", communityController.approveCommunity);
-router.get("/files/:folder/:id", utilsController.getFiles);
-router.post("/create", communityController.createCommunity);
 router.get("/change-user-type/:id", communityController.changeAccountType);
+router.get("/files/:folder/:id", utilsController.getFiles);
+router.post("/upload-community", utilsController.uploadPostImage);
+router.post("/create", communityController.createCommunity);
 router.post("/create-community-admin", communityController.createCommunityAdmin);
 router.delete("/:id", communityController.deleteCommunity);
 
