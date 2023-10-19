215cffedd4b8924b52f7e995c75ffbcf5d0f17dd
Swapnil Opash <sra.opash@gmail.com>
2023-10-19 10:16:27 +0530

    changed email cred

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -4,16 +4,16 @@ const path = require("path");
 const environment = require("../environments/environment");
 
 let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: { user: environment.SMTP_USER, pass: environment.SMTP_PASS },
+  // service: "gmail",
+  // auth: { user: environment.SMTP_USER, pass: environment.SMTP_PASS },
   // tls: {
   //   host: "freedom.social",
   //   port: 993,
   // },
-  // host: "smtp.freedom.social",
-  // port: 465,
-  // sender: "no-reply@freedom.buzz",
-  // auth: { user: environment.SMTP_USER, pass: environment.SMTP_PASS },
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  sender: "no-reply@freedom.buzz",
+  auth: { user: environment.SMTP_USER, pass: environment.SMTP_PASS },
 });
 
 exports.sendMail = async function (mailObj) {
