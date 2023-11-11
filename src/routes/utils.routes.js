cd33da48c9e231f062e236e06e7e8154b8dbde1d
Swapnil Opash <sra.opash@gmail.com>
2023-11-11 10:39:40 +0530

    fixed upload profile issue

--- src/routes/utils.routes.js
+++ src/routes/utils.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const utilsController = require("../controllers/utils.controller");
 
-// router.post("/upload", utilsController.fileupload);
+router.post("/upload", utilsController.fileupload);
 // router.post("/upload2", utilsController.fileupload2);
 router.get("/files/:folder/:id", utilsController.getFiles);
 router.get("/download/partner/:name", utilsController.downloadPartner);
