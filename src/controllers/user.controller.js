1858e12fa7f830339038071f3390d7a5ae5a1121
Swapnil Opash <sra.opash@gmail.com>
2023-12-01 14:48:57 +0530

    Revert "changed logout api"
    This reverts commit 2212ef294b3ee282396e1bba6f46517dc5dde75c.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -465,13 +465,12 @@ exports.resendVerification = function (req, res) {
 
 exports.logout = function (req, res) {
   console.log("cookies");
-  res.cookie("auth-user", "Hello", {
+  res.cookie("auth-user", 'Hello', {
     expire: new Date(Date.now() - 900000),
     secure: true,
     sameSite: "none",
     domain: environments.domain,
   });
-  req.clearCookie("auth-user", { domain: environments.domain, path: "/" });
-  res.clearCookie("auth-user", { domain: environments.domain, path: "/" });
+  res.clearCookie("auth-user");
   res.end();
 };
