556e4a9391861ba6619fd87a4ac3990b71aeffb1
Swapnil Opash <sra.opash@gmail.com>
2024-01-31 18:55:20 +0530

    fixed

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -135,7 +135,7 @@ Profile.getUsersByUsername = async function (searchText) {
 
 Profile.getNotificationById = async function (id) {
   if (id) {
-    const query = `select n.*,p.Username,p.FirstName,p.ProfilePicName from notifications as n left join profile as p on p.ID = n.notificationByProfileId where n.notificationToProfileId =? and n.actionType not in ('VC','DC') order by createDate desc`;
+    const query = `select n.*,p.Username,p.FirstName,p.ProfilePicName from notifications as n left join profile as p on p.ID = n.notificationByProfileId where n.notificationToProfileId =? order by createDate desc`;
     const values = [id];
     const notificationData = await executeQuery(query, values);
     return notificationData;
