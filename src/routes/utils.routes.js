c4659ebc32f408bb31dd6ed1549a434a4b48f6a1
Swapnil Opash <sra.opash@gmail.com>
2024-02-21 19:22:19 +0530

    fixed

--- src/routes/utils.routes.js
+++ src/routes/utils.routes.js
@@ -11,11 +11,11 @@ router.post(
 );
 
 router.get("/files/:folder/:id", utilsController.getFiles);
-router.use(authorize);
+router.get("/:folder/:id/:filename", utilsController.readFile);
+// router.use(authorize);
 router.post("/upload", utilsController.fileupload);
 // router.post("/upload2", utilsController.fileupload2);
 router.get("/download/partner/:name", utilsController.downloadPartner);
 router.get("/download/:folder/:id/:name", utilsController.download);
-router.get("/:folder/:id/:filename", utilsController.readFile);
 
 module.exports = router;
