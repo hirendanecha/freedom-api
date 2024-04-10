241790882c07502881e6af50505e5588b8c4c717
Swapnil Opash <sra.opash@gmail.com>
2024-04-10 17:18:44 +0530

    fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -133,7 +133,7 @@ User.findAndSearchAll = async (limit, offset, search, startDate, endDate) => {
     `SELECT count(Id) as count FROM users as u WHERE ${whereCondition}`
   );
   const searchData = await executeQuery(
-    `SELECT u.Id, u.Email, u.Username, u.IsActive, u.DateCreation, u.IsAdmin, u.FirstName, u.LastName, u.Address, u.Country, u.City, u.State, u.Zip, u.AccountType, u.IsSuspended,p.MobileNo,p.ProfilePicName,p.ID as profileId,p.MediaApproved FROM users as u left join profile as p on p.UserID = u.Id  WHERE ${whereCondition} order by DateCreation desc limit ? offset ?`,
+    `SELECT u.Id, u.Email, u.Username, u.IsActive, u.DateCreation, u.IsAdmin, u.FirstName, u.LastName, u.Address, u.Country, u.City, u.State, u.Zip, u.AccountType, u.IsSuspended,p.MobileNo,p.ProfilePicName,p.ID as profileId,p.MediaApproved FROM users as u left join profile as p on p.UserID = u.Id and p.AccountType IN ('I','M') WHERE ${whereCondition} order by DateCreation desc limit ? offset ?`,
     [limit, offset]
   );
 
