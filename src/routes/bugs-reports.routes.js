31cc624c6da9ddd5bcadcf8e10374cdd8f6b79f8
Swapnil Opash <sra.opash@gmail.com>
2024-05-27 12:37:05 +0530

    fixed

--- src/routes/bugs-reports.routes.js
+++ src/routes/bugs-reports.routes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const bugsAndReportsController = require("../controllers/bugs-reports.controller");
 const authorize = require("../middleware/authorize");
 
+router.post("/support-contact", bugsAndReportsController.supportContact);
 router.use(authorize.authorization);
 router.get("/:id", bugsAndReportsController.getBugDetails);
 router.post("/", bugsAndReportsController.getBugsList);
 router.post("/add-bugs", bugsAndReportsController.addBugReports);
-router.post("/support-contact", bugsAndReportsController.supportContact);
 router.put("/change-status", bugsAndReportsController.updateBugsStatus);
 router.delete("/delete/:id", bugsAndReportsController.deleteBugs);
 
